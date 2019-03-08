2edce20887255ae0474d7607cab2b1ac0b9da9da
Pavel <alwayssanmw@gmail.com>
2019-03-08 16:43:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,10 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
+// for heroku
+// server.listen(process.env.PORT || 5000, () => {
+//     console.log('Listening for requests now');
+// });
 
 
 
